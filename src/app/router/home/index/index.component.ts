1ce422b337cc7cd944bec823bc82a94605006bba
TT <tt-tjw@qq.com>
2019-05-11 20:47:52 +0800

    modify

--- src/app/router/home/index/index.component.ts
+++ src/app/router/home/index/index.component.ts
@@ -12,11 +12,17 @@ import { AppState } from "src/app/store/app.reducer";
 export class IndexComponent implements OnInit {
   current: string = "";
 
-  @ViewChild("unit") unit: UnitComponent;
+  units = {
+    unit1: `Unit1 Let's count! Look! Coll! How many red marbles? One, two, three. Three red marbles. How many green marbles? One, two, three, four. Four green marbles. How many yellow marbles? One, two, three, four, five. Five yellow marbles.`,
+    unit2: `Unit2 This is my pencil Ouch! I'm sorry, Yang Ling. That's OK. Is this your book, Liu Tao? Yes. Thank you, Su Hai. Is this your pencil, Yang Ling? No. Oh, this is my pencil! Ha! Ha! a book a pencil a ruler a rubber`,
 
-  text = `Unit1 Let's count! Look! Coll! How many red marbles? One, two, three. Three red marbles. How many green marbles? One, two, three, four. Four green marbles. How many yellow marbles? One, two, three, four, five. Five yellow marbles.`;
+    unit3: `Unit3 I like carrots I like carrots. Me too. Onions? No, thanks. I like onions. Peas? Yes, please. We all like peas. carrots onions peas peppers`,
 
-  unit2 = `Unit2 This is my pencil Ouch! I'm sorry, Yang Ling. That's OK. Is this your book, Liu Tao? Yes. Thank you, Su Hai. Is this your pencil, Yang Ling? No. Oh, this is my pencil! Ha! Ha! a book a pencil a ruler a rubber`;
+    unit4: `Unit4 Spring Look at the trees. They're green. Look at the flowers. They're beautiful. Look at the birds. They're happy. Look at the kites. They're colourful. a tree a flower a bird a kite`,
+    unit5: `TODO`,
+    unit6: `TODO`,
+    unit7: `TODO`
+  };
 
   app$: Observable<AppState>;
   constructor(public store: Store<any>) {
@@ -27,7 +33,7 @@ export class IndexComponent implements OnInit {
     // this.text = this.text.replace("\r", "");
     // this.text = this.text.replace("\n", "");
     // this.text = this.text.replace("\t", "");
-    this.store.dispatch({ type: "SET_TEXT", payload: this.unit2 });
+    this.store.dispatch({ type: "SET_TEXT", payload: this.units.unit3 });
   }
 
   ngOnInit() {}
@@ -44,6 +50,10 @@ export class IndexComponent implements OnInit {
 
   input(input: string) {
     this.store.dispatch({ type: "INPUT", payload: input });
+  }
+
+  setText(text: string) {
+    this.store.dispatch({ type: "SET_TEXT", payload: text });
   }
 
   next() {
--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -2,13 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   template: `
-    <mat-sidenav-container class="example-container">
-      <mat-sidenav mode="side" opened>Sidenav content</mat-sidenav>
-      <mat-sidenav-content>
-        <router-outlet></router-outlet>
-      </mat-sidenav-content>
-    </mat-sidenav-container>
-
+    <router-outlet></router-outlet>
   `
 })
 export class LayoutComponent implements OnInit {
